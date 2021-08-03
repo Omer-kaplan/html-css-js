d3342f96cce49db3c76220d95f5b4d090b276947
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2021-08-03 11:03:22 +0300

    comments-the-code

--- tira-proj/index.js
+++ tira-proj/index.js
@@ -1,11 +1,11 @@
 const headere = document.querySelectorAll("p")
-const newe = document.querySelector(".new")
+/*const newe = document.querySelector(".new") */
 
 const alertfunc= (e) => {
-    // window.alert("hello world");
-    let div = document.createElement("li");
+    window.alert("Have an alert when pressing a Paragraphs");
+    /* let div = document.createElement("li");
     div.innerText="asfdfsdfsdfsdfsdfsdfsdfsdf";
-    newe.appendChild(div)
+    newe.appendChild(div)*/
 }
 headere.forEach(item=>{
     item.addEventListener("click", alertfunc);
