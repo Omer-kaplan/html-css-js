9d715f3ecc89d3671c2f0649103554ed200209da
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2021-08-05 17:53:14 +0300

    making-tira-better

---
+++ testing-JS/scripting.js
@@ -0,0 +1,37 @@
+/* --- Main --- /
+
+let systemCards = {
+    name : "---Dynamic---",
+    currentImg : "---Dynamic"
+}
+
+console.log(systemCards);
+/* --------------------------------------------------------------------------/
+
+let listSimple = ['omer', 'Kaplan', 'infraDev', 'Adri', 'Mor'];
+// console.log(listSimple[0]) == omer
+listSimple.push('hahaLOL');
+
+
+let numList = [1,2,3,4,5,6];
+let indexer;
+for (indexer = 0; indexer < numList.length; indexer++)
+{
+    console.log('The position is --> ' + indexer + ' Value ' + numList[indexer]);
+}
+
+/* --------------------------------------------------------------------------/
+
+let n1 = 10, n2 = 100;
+function sum(num1, num2){
+    console.log(num1 + num2);
+}
+sum(n1,n2);
+
+/ --------------------------------------------------------------------------/
+
+// console.log(document.getElementById("footer-style"));
+// console.log(document.getElementsByClassName('main-headers'));
+// console.log(document.getElementsByTagName("img"));
+
+/ -------------------------------------------------------------------------- */
